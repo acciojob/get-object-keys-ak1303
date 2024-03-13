be8133b17a9078d5d63dc331429cb2f12eea9e21
ak1303 <94529241+ak1303@users.noreply.github.com>
2024-03-13 16:40:29 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,11 @@
 //your JS code here. If required.
+obj student = {
+	name:'Abhishek'
+}
+Object.prototype.getKeys = function() {
+	let arr=[];
+	for(let i in this){
+		arr.push(i);
+	}
+	return arr;
+}
